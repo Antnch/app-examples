a71d70ebf3610a918bbe319e45c1eec896add173
antnch <anton.n@miro.com>
2020-02-13 22:14:17 +0500

    Delete excess

--- font-awesome/README.md
+++
@@ -1,3 +0,0 @@
-# Font Awesome plugin
-
-This plugin lets you use icons from [Font Awesome](https://fontawesome.com/) on your Miro boards.
--- plugin-boilerplate/src/modal.ts
+++
@@ -1,6 +0,0 @@
-require('./styles.less')
-
-let closeButton = document.querySelector('.close-button')!
-closeButton.addEventListener('click', () => {
-	miro.board.ui.closeModal()
-})
